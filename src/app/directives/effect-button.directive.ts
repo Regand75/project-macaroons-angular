6984a4d64beb08ec1190543e3d7b982ccbab4faf
regand75 <regandess@gmail.com>
2025-01-20 19:37:43 +0300

    finished directives

--- src/app/directives/effect-button.directive.ts
+++ src/app/directives/effect-button.directive.ts
@@ -1,12 +1,28 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[effectButton]'
 })
-export class EffectButtonDirective {
+export class EffectButtonDirective implements OnInit{
 
-  constructor(el: ElementRef) {
-    console.log(el);
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.changeElementStyleValue('box-shadow', '0 5px 9px 1px rgba(130, 19, 40, 0.5)');
   }
 
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.changeElementStyleValue('box-shadow', 'none');
+  }
+
+  ngOnInit() {
+    this.changeElementStyleValue('background-image', 'linear-gradient(90deg, rgb(113, 8, 30) 0%, rgb(215, 72, 92) 100%)');
+    this.changeElementStyleValue('box-shadow', 'none');
+  }
+
+  changeElementStyleValue(style: string, value: string) {
+    this.renderer.setStyle(this.el.nativeElement, style, value);
+  }
 }
